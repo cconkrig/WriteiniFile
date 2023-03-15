a605dde0200f3c67807a60e4b0d1d23b80537871
Chris Conkright <2049188+cconkrig@users.noreply.github.com>
2023-03-15 19:29:44 -0400

    Added better custom php ini parser
    Increased package sustainability and usability by removing PHP native parser that cripples ini file functionality.

--- src/ReadiniFile.php
+++ src/ReadiniFile.php
@@ -18,19 +18,74 @@ class ReadiniFile
     protected static $data_ini_file;
 
     /**
+    * method for parsing data in ini file
+    * This is a much better solution than the limited abilities of php's ini parser
+    * In the php native function if you have ; in your value, it gets truncated.
+    * Systems already in use cannot simple escape the data before saving it.
+    * PHP does not follow the guidlines for ini values which including allowing ; in the value
+    * This function remedies this issue.
+    *
+    * @param string $filepath     path of ini file
+    *
+    * @return array ini file data in a array
+    */
+    private function better_parse_ini ( $filepath ) {
+        $ini = file( $filepath );
+        if ( count( $ini ) == 0 ) { return array(); }
+        $sections = array();
+        $values = array();
+        $globals = array();
+        $i = 0;
+        foreach( $ini as $line ){
+            $line = trim( $line );
+            // Comments
+            if ( $line == '' || $line[0] == ';' ) { continue; }
+            // Sections
+            if ( $line[0] == '[' ) {
+                $sections[] = substr( $line, 1, -1 );
+                $i++;
+                continue;
+            }
+            // Key-value pair
+            list( $key, $value ) = explode( '=', $line, 2 );
+            $key = trim( $key );
+            $value = trim( $value );
+            if ( $i == 0 ) {
+                // Array values
+                if ( substr( $line, -1, 2 ) == '[]' ) {
+                    $globals[ $key ][] = $value;
+                } else {
+                    $globals[ $key ] = $value;
+                }
+            } else {
+                // Array values
+                if ( substr( $line, -1, 2 ) == '[]' ) {
+                    $values[ $i - 1 ][ $key ][] = $value;
+                } else {
+                    $values[ $i - 1 ][ $key ] = $value;
+                }
+            }
+        }
+        for( $j=0; $j<$i; $j++ ) {
+            $result[ $sections[ $j ] ] = $values[ $j ];
+        }
+        return $result + $globals;
+    }
+    
+    
+    /**
      * method for get data of ini file.
      *
      * @param string $ini_file     path of ini file
-     * @param int    $scanner_mode scanner mode INI_SCANNER_RAW INI_SCANNER_TYPED or INI_SCANNER_NORMAL
      *
      * @return array ini file data in a array
      */
-    public static function get($ini_file, $scanner_mode = INI_SCANNER_RAW)
+    public static function get($ini_file)
     {
         self::$path_to_ini_file = $ini_file;
 
         if (file_exists(self::$path_to_ini_file) === true) {
-            self::$data_ini_file = @parse_ini_file(self::$path_to_ini_file, true, $scanner_mode);
+            self::$data_ini_file = @better_parse_ini(self::$path_to_ini_file);
         } else {
             throw new \Exception(sprintf('File ini does not exist: %s', self::$path_to_ini_file));
         }
